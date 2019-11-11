26411a894d53e0995a38a00293ce5734ddb2d25d
ppphp <kevinniub@gmail.com>
2019-11-12 00:47:07 +0800

    update

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"io/ioutil"
 	"log"
 	"os"
@@ -14,6 +15,7 @@ import (
 type Command struct {
 	watcher *quartz.Quartz
 	pwd     string
+	cmd     *exec.Cmd
 }
 
 func (c *Command) Help() string {
@@ -46,11 +48,28 @@ func (c *Command) Run(args []string) int {
 			if err != nil {
 			}
 			f.Close()
-			cmd := exec.Command("go", "build", "-o", f.Name(), "./cmd/...")
+			cmd := exec.Command("go", "build", "-o", f.Name(), "./cmd/yayagf")
+			var o, e bytes.Buffer
+			cmd.Stdout = &o
+			cmd.Stderr = &e
 			if err := cmd.Run(); err != nil {
-				log.Printf("build to %v err: %v\n", f.Name(), err)
+				log.Printf("build to %v err: %v, err: %v, out: %v\n", f.Name(), err, e.String(), o.String())
 				continue
 			}
+			if c.cmd != nil && c.cmd.Process != nil {
+				if err := c.cmd.Process.Kill(); err != nil {
+					log.Printf("kill %v err: %v", c.cmd.Process.Pid, err)
+				}
+			}
+			c.cmd = exec.Command(f.Name())
+			go func() {
+				var o, e bytes.Buffer
+				c.cmd.Stdout = &o
+				c.cmd.Stderr = &e
+				if err := c.cmd.Run(); err != nil {
+					log.Printf("run %v err: %v, err: %v, out: %v\n", f.Name(), err, e.String(), o.String())
+				}
+			}()
 		}
 	}
 
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/mitchellh/cli v1.0.0
 	github.com/ppphp/quartz v0.0.0-20191111105708-ee277a6130a3
 )
+
+replace github.com/ppphp/quartz => ../quartz
