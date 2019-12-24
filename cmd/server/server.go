7d33737ef0a4802bd7a94de47be225145da6f244
fengche <2395434298@qq.com>
2019-12-24 13:41:32 +0800

    经常碰到老端口没释放的问题，之前的进程退出了就总是重启

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -61,20 +61,22 @@ func (c *Command) Run(args []string) int {
 				log.Printf("build to %v err: %v, err: %v, out: %v\n", f.Name(), err, e.String(), o.String())
 				continue
 			}
-			f1, err1 := ioutil.ReadFile(f.Name())
-			if err1 != nil {
-				continue
-			}
-			if lastName != "" {
-				f2, err2 := ioutil.ReadFile(lastName)
-				if err2 != nil {
+			if !c.cmd.ProcessState.Exited() {
+				f1, err1 := ioutil.ReadFile(f.Name())
+				if err1 != nil {
 					continue
 				}
-				if bytes.Equal(f1, f2) {
-					continue
+				if lastName != "" {
+					f2, err2 := ioutil.ReadFile(lastName)
+					if err2 != nil {
+						continue
+					}
+					if bytes.Equal(f1, f2) {
+						continue
+					}
 				}
+				lastName = f.Name()
 			}
-			lastName = f.Name()
 			if c.cmd != nil && c.cmd.Process != nil {
 				if err := c.cmd.Process.Kill(); err != nil {
 					log.Printf("kill %v err: %v", c.cmd.Process.Pid, err)
