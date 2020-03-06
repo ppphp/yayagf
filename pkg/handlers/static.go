b539f60b83e85c17976f4e8a6a89f4b8d8e295d8
fengche <2395434298@qq.com>
2020-03-06 18:51:43 +0800

    fix path

--- pkg/handlers/static.go
+++ pkg/handlers/static.go
@@ -4,19 +4,20 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-func ServeStaticDirectory(path string) ([]Handler, error) {
-	path = filepath.Clean(path)
+func ServeStaticDirectory(dir string) ([]Handler, error) {
+	dir = filepath.Clean(dir)
 	hs := []Handler{}
-	if err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+	if err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if info.IsDir() {
 			return nil
 		}
-		hs = append(hs, Handler{path: path, handler: http.FileServer(http.Dir(path))})
+		hs = append(hs, Handler{path: strings.TrimPrefix(path, dir), handler: http.FileServer(http.Dir(path))})
 		return nil
 	}); err != nil {
 		return nil, err
