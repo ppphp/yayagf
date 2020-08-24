463638b4780ccd8d08c55978d92b4fa186ffb788
fengche <2395434298@qq.com>
2020-08-24 19:40:46 +0800

    feat: some meta update

--- pkg/meta/meta.go
+++ pkg/meta/meta.go
@@ -2,12 +2,49 @@
 package meta
 
 import (
+	"net"
+	"os"
 	"runtime"
 	"runtime/debug"
+	"strings"
+	"time"
 )
 
 var BuildInfo, _ = debug.ReadBuildInfo()
 var binhash, _ = CalculateSelfMD5()
+var uptime = time.Now()
+var mtime = func() time.Time {
+	st, err := os.Stat(os.Args[0])
+	if err != nil {
+		return time.Now()
+	}
+	return st.ModTime()
+}()
+
+var local = func() string {
+	locals, err := net.Interfaces()
+	if err != nil {
+		panic("cannot find local address")
+	}
+	for _, interfac := range locals {
+		if interfac.HardwareAddr.String() != "" {
+			if strings.HasPrefix(interfac.Name, "en") ||
+				strings.HasPrefix(interfac.Name, "eth") {
+				if addrs, err := interfac.Addrs(); err == nil {
+					for _, addr := range addrs {
+						if addr.Network() == "ip+net" {
+							pr := strings.Split(addr.String(), "/")
+							if len(pr) == 2 && len(strings.Split(pr[0], ".")) == 4 {
+								return pr[0]
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	panic("cannot find local address")
+}()
 
 // 一个构建期注入的meta信息结构体
 var Meta = struct {
@@ -18,7 +55,10 @@ var Meta = struct {
 	GoCompiler  string
 	GoBuildInfo *debug.BuildInfo
 
-	MD5 string
+	MD5    string
+	Uptime time.Time
+	Mtime  time.Time
+	Local  string
 	// 注入
 	BuildAt string
 	Commit  string
@@ -32,4 +72,7 @@ var Meta = struct {
 	Commit:      "HEAD",
 	Version:     "HEAD",
 	MD5:         binhash,
+	Uptime:      uptime,
+	Mtime:       mtime,
+	Local:       local,
 }
