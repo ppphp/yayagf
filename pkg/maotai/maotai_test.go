12186901650e7f75528319346002304dc172d408
fengche <2395434298@qq.com>
2020-09-24 17:37:45 +0800

    chore: add test

---
+++ pkg/maotai/maotai_test.go
@@ -0,0 +1,58 @@
+package maotai
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	assert.NotNil(t, New())
+}
+
+func TestDefault(t *testing.T) {
+	assert.NotNil(t, Default("test"))
+}
+
+func TestNikkiSerializer(t *testing.T) {
+	d := Default("test")
+	r := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(r)
+	c.Request, _ = http.NewRequest(http.MethodGet, "http://0.0.0.0", nil)
+	NikkiSerializer(d, func(c *Context) (int, string, gin.H) { return 0, "", gin.H{"a": "b"} })(c)
+	assert.NotEmpty(t, r.Result())
+}
+
+func TestPlainSerializer(t *testing.T) {
+	d := Default("test")
+	r := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(r)
+	c.Request, _ = http.NewRequest(http.MethodGet, "http://0.0.0.0", nil)
+	PlainSerializer(d, func(c *Context) (int, string, interface{}) { return 0, "", nil })(c)
+	assert.NotEmpty(t, r.Result())
+}
+
+func TestTDSSerializer(t *testing.T) {
+	d := Default("test")
+	r := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(r)
+	c.Request, _ = http.NewRequest(http.MethodGet, "http://0.0.0.0", nil)
+	TDSSerializer(d, func(c *Context) (int, string, gin.H) { return 0, "", gin.H{"a": "b"} })(c)
+	assert.NotEmpty(t, r.Result())
+}
+
+func TestMethod(t *testing.T) {
+	d := Default("test")
+	d.Use()
+	d.Handle("GET", "/1/")
+	d.GET("/2/")
+	d.POST("/3/")
+	d.PUT("/4/")
+	d.DELETE("/5/")
+	d.PATCH("/6/")
+	d.OPTIONS("/6/")
+	d.HEAD("/6/")
+}
