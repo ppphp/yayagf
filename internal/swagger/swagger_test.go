d77dbf6ac2d271fc03c5b90f9bf34cd0002bf9fb
fengche <2395434298@qq.com>
2020-09-24 17:10:15 +0800

    chore: add test

--- internal/swagger/swagger_test.go
+++ internal/swagger/swagger_test.go
@@ -1,1 +1,15 @@
 package swagger
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateSwagger(t *testing.T) {
+
+	assert.NoError(t, os.Chdir("testdata"))
+	assert.NoError(t, GenerateSwagger())
+	assert.NoError(t, os.Chdir("../"))
+}
---
+++ internal/swagger/testdata/go.mod
@@ -0,0 +1,3 @@
+module test.com/testdata
+
+go 1.14
