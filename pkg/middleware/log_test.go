b53891214f5d80c37f6d8200467a20c573e362d7
fengche <2395434298@qq.com>
2020-11-24 15:50:11 +0800

    chore: cover middleware

--- pkg/middleware/log_test.go
+++ pkg/middleware/log_test.go
@@ -1,11 +1,45 @@
 package middleware
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGinrus(t *testing.T) {
-	Ginrus(logrus.New())
+	re := gin.Default()
+	g := Ginrus(logrus.New())
+	require.NotNil(t, g)
+
+	re.Use(g)
+	re.GET("/", func(c *gin.Context) {
+		_, _ = ioutil.ReadAll(c.Request.Body)
+	})
+	re.GET("/bad", func(c *gin.Context) {
+		_ = c.Error(fmt.Errorf("aa"))
+	})
+
+	t.Run("ok", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		r, err := http.NewRequest(http.MethodGet, "http://0.0.0.0:8080/", &bytes.Buffer{})
+		require.NoError(t, err)
+		re.ServeHTTP(rr, r)
+		require.NotEqual(t, "", rr.Result())
+	})
+
+	t.Run("bad", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		r, err := http.NewRequest(http.MethodGet, "http://0.0.0.0:8080/bad", &bytes.Buffer{})
+		require.NoError(t, err)
+		re.ServeHTTP(rr, r)
+		require.NotEqual(t, "", rr.Result())
+	})
+
 }
