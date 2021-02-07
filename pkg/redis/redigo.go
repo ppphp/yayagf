004040dd88958092c84faa85424c03d481013017
fengche <2395434298@qq.com>
2021-02-07 20:49:15 +0800

    fix: dup codes

--- pkg/redis/redigo.go
+++ pkg/redis/redigo.go
@@ -8,28 +8,7 @@ import (
 var RedisPool *redis.Pool
 
 func InitRedisPool(addr, key string) {
-	RedisPool = &redis.Pool{
-		MaxIdle:     100,
-		IdleTimeout: 60 * time.Second,
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", addr)
-			if err != nil {
-				return nil, err
-			}
-			if key == "" {
-				return c, nil
-			}
-			if _, err := c.Do("AUTH", key); err != nil {
-				c.Close()
-				return nil, err
-			}
-			return c, nil
-		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			_, err := c.Do("PING")
-			return err
-		},
-	}
+	RedisPool = GenRedisPool(addr, key)
 }
 
 func GenRedisPool(addr, key string) *redis.Pool {
